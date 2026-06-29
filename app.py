b0796daf3a15185214f922cb4ef9f0e00ba0671e
minum2387-debug <minum2387@gmail.com>
2026-06-29 11:55:48 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import sklearn as sk
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # Load dataset
